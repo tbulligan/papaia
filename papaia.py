94e4866e5be03dc4a3b8da9e7569a1ee54611627
Tomaso Bulligan <tomaso.bulligan@gmail.com>
2025-03-14 11:16:59 +0100

    Test .gitignore.

---
+++ papaia.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
